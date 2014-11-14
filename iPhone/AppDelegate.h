e6b5d29631e53447a06f5621d6336fb85e5d52b2
Marc Landolt jun <marc@Marcs-Mac-Pro.local>
2014-11-14 03:58:38 -0800

    Initial Commit

---
+++ iPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhone
+//
+//  Created by Marc Landolt jun. on 14.11.14.
+//  Copyright (c) 2014 Marc Landolt jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPhone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPhone
+//
+//  Created by Marc Landolt jun. on 14.11.14.
+//  Copyright (c) 2014 Marc Landolt jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
